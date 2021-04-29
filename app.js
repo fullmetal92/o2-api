570fc81bfae09b24d1003a62f19078371e818905
Deepak Sharma <deepak@loopintegration.com>
2021-04-29 23:50:09 +0530

    Configured error handler

--- app.js
+++ app.js
@@ -47,7 +47,10 @@ app.use(function(err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.json({
+        code: err.status,
+        error: err.message
+    });
 });
 
 module.exports = app;
---
+++ config.js
@@ -0,0 +1,81 @@
+/**
+ * Project Astro configuration
+ * Created by fatal1ty on 8/6/17.
+ */
+
+const config = {
+
+    // Production
+    production: {
+
+        api: {
+            baseUrl: 'https://json.astrologyapi.com/v1',
+            username: '4545',
+            password: 'f9b007c05c0df06c0df3082b9a965119',
+            endpoints: {
+                manglik: '/manglik'
+            }
+        },
+
+        database: {
+            name: '',
+            username: '',
+            password: ''
+        },
+
+        security: {
+            jwt: {
+                scheme: 'JWT',
+                secret: 'mySecret',
+                issuer: 'localhost',
+                audience: 'localhost',
+                expiresIn: '5m' // expires in 5 minutes (See: https://github.com/zeit/ms)
+            }
+        },
+
+        logger: {
+            dirName: 'log',
+            extension: '.log',
+            dateFormat: 'DD-MM-YYYY'
+        }
+    },
+    default: {
+
+        api: {
+            baseUrl: 'https://json.astrologyapi.com/v1',
+            username: '4545',
+            password: 'f9b007c05c0df06c0df3082b9a965119',
+            endpoints: {
+                manglik: '/manglik'
+            }
+        },
+
+        database: {
+            url: 'mongodb://127.0.0.1:27017/astro-db',
+            options: {
+                user: 'astro',
+                pass: 'astro'
+            }
+        },
+
+        security: {
+            jwt: {
+                scheme: 'JWT',
+                secret: 'mySecret',
+                issuer: 'localhost',
+                audience: 'localhost',
+                expiresIn: '60m' // expires in 5 minutes (See: https://github.com/zeit/ms)
+            }
+        },
+
+        logger: {
+            dirName: 'log',
+            extension: '.log',
+            dateFormat: 'DD-MM-YYYY'
+        }
+    }
+};
+
+exports.get = function get(env) {
+    return config[env] || config.default;
+};
