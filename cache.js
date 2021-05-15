2d9e0e35b5ccbb8dfdfe70f19c947a4e67779592
Deepak Sharma <deepak@loopintegration.com>
2021-05-15 23:46:27 +0530

    Replaced redis config params with redis url

--- cache.js
+++ cache.js
@@ -4,12 +4,7 @@ const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
 const winston = require('./logger');
-const client = redis.createClient({
-    host: process.env.REDIS_HOST || 'localhost',
-    port: process.env.REDIS_PORT || 6379,
-    user: process.env.REDIS_USER || 'default',
-    password: process.env.REDIS_PASSWORD
-});
+const client = redis.createClient(process.env.REDIS_URL);
 
 client.hget = util.promisify(client.hget);
 
