5e9e95b66a709589ef7a234c7af2936566580310
Deepak Sharma <deepak@loopintegration.com>
2021-05-16 10:12:21 +0530

    Added back redis config with params

--- cache.js
+++ cache.js
@@ -4,7 +4,12 @@ const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
 const winston = require('./logger');
-const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient({
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379,
+    user: process.env.REDIS_USER,
+    password: process.env.REDIS_PASSWORD
+});
 
 client.hget = util.promisify(client.hget);
 
